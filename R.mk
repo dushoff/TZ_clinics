c32e9d50a2757909bd29408ab7a56bac3e66ac29
Jonathan Dushoff <dushoff@mcmaster.ca>
2015-09-10 22:58:00 -0400

    Autocommit (TZ_clinics) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   .gitignore
    	modified:   R.mk
    	new file:   keep.R

--- R.mk
+++ R.mk
@@ -1,7 +1,20 @@
 
+### Make a subsample for quicker comparison 
+
 Sources += sample.R
 sample.Rout: children_visits.RData sample.R
 	$(run-R)
 
 sample.RData:
+
+### Keep far too many variables :-)
+
+Sources += keep.R
+keep.sample.RData keep.visits.RData:
+
+keep.sample.Rout: sample.Rout keep.R
+	$(run-R)
+
+keep.visits.Rout: children_visits.Rout keep.R
+	$(run-R)
 
