5f6d8510d558c37b23d9bad1d4d4dc8517e1f457
mikelin2 <wzmichael.li@gmail.com>
2015-10-05 13:38:09 -0400

    cleaned survival

--- survival.R
+++ survival.R
@@ -2,62 +2,48 @@
 
 library(survival)
 
-zerohack <- (Datetable %>% mutate(startdate = firstVisit - 1,
+TZsurdat <- (Datetable %>% mutate(startdate = firstVisit - 1,
                                   hack1 = firstVisit -1, 
                                  hack20 = firstVisit - 20)
 )
 
-Surhack1 <- (survfit(Surv(as.numeric(arv_date)-as.numeric(hack1),!is.na(arv_date))~1,data=zerohack))
+fit <- coxph(Surv(as.numeric(arv_date)-as.numeric(hack1),!is.na(arv_date))~1,data=TZsurdat)
+fit2 <- coxph(Surv(as.numeric(arv_date)-as.numeric(hack20), !is.na(arv_date))~1, data=TZsurdat)
 
-Surhack20 <- (survfit(Surv(as.numeric(arv_date)-as.numeric(hack20),!is.na(arv_date))~1,data=zerohack))
+summary(fit)
+summary(fit2)
 
-
-plot(Surhack1, col=1)
-lines(Surhack20, col=2)
-
-##?!? plots don't overlap
-
-fit <- coxph(Surv(as.numeric(arv_date)-as.numeric(hack1),!is.na(arv_date))~1,data=zerohack)
-fit2 <- coxph(Surv(as.numeric(arv_date)-as.numeric(hack20), !is.na(arv_date))~1, data=zerohack)
-
-
-plot(survfit(fit))
-plot(survfit(fit),xlim=c(0,50))
-lines(SurARV,col=2)
-lines(survfit(fit2),col=4)
-
-
-##?!? still don't overlap??
+##good ! it doesn't matter how many days we hack
 
 ##first do table 3 from Joseph's docx
 
 ARTeligi <- function(cat,logic){
   data.frame(Factor = cat,
-             "<2" = nrow(subset(zerohack,logic & zerohack$age < 1)),
-             "2-5" = nrow(subset(zerohack,logic & zerohack$age >= 2 & zerohack$age <= 5)),
-             "6-9" = nrow(subset(zerohack,logic & zerohack$age >= 6 & zerohack$age <= 9)),
-             "10-14" = nrow(subset(zerohack,logic & zerohack$age >= 10 & zerohack$age <= 14)),
-             Total = nrow(subset(zerohack,logic)))
+             "<2" = nrow(subset(TZsurdat,logic & TZsurdat$age < 1)),
+             "2-5" = nrow(subset(TZsurdat,logic & TZsurdat$age >= 2 & TZsurdat$age <= 5)),
+             "6-9" = nrow(subset(TZsurdat,logic & TZsurdat$age >= 6 & TZsurdat$age <= 9)),
+             "10-14" = nrow(subset(TZsurdat,logic & TZsurdat$age >= 10 & TZsurdat$age <= 14)),
+             Total = nrow(subset(TZsurdat,logic)))
   
 }
 
 time_ARTenrolment <- (rbind(
-  ARTeligi("Enrolment", !is.na(zerohack$arv_date))
-  ,ARTeligi("Within 1 month",(as.numeric(zerohack$arv_date)- as.numeric(zerohack$startdate)) <= 30)
-  ,ARTeligi("Within 2 month",(as.numeric(zerohack$arv_date)- as.numeric(zerohack$startdate)) <= 60)
-  ,ARTeligi("Within 3 month",(as.numeric(zerohack$arv_date)- as.numeric(zerohack$startdate)) <= 90)
-  ,ARTeligi("Within 6 month",(as.numeric(zerohack$arv_date)- as.numeric(zerohack$startdate)) <= 180)
-  ,ARTeligi("Within 12 month",(as.numeric(zerohack$arv_date)- as.numeric(zerohack$startdate)) <= 365)
-  ,ARTeligi("> 12 month",(as.numeric(zerohack$arv_date)- as.numeric(zerohack$startdate)) > 365)
-  ,ARTeligi("Not yet started", is.na(zerohack$arv_date))
+  ARTeligi("Enrolment", !is.na(TZsurdat$arv_date))
+  ,ARTeligi("Within 1 month",(as.numeric(TZsurdat$arv_date)- as.numeric(TZsurdat$startdate)) <= 30)
+  ,ARTeligi("Within 2 month",(as.numeric(TZsurdat$arv_date)- as.numeric(TZsurdat$startdate)) <= 60)
+  ,ARTeligi("Within 3 month",(as.numeric(TZsurdat$arv_date)- as.numeric(TZsurdat$startdate)) <= 90)
+  ,ARTeligi("Within 6 month",(as.numeric(TZsurdat$arv_date)- as.numeric(TZsurdat$startdate)) <= 180)
+  ,ARTeligi("Within 12 month",(as.numeric(TZsurdat$arv_date)- as.numeric(TZsurdat$startdate)) <= 365)
+  ,ARTeligi("> 12 month",(as.numeric(TZsurdat$arv_date)- as.numeric(TZsurdat$startdate)) > 365)
+  ,ARTeligi("Not yet started", is.na(TZsurdat$arv_date))
 ))
 time_ARTenrolment
 
-SurARV <- Surhack1
+SurARV <- survfit(Surv(as.numeric(arv_date)-as.numeric(startdate), !is.na(arv_date))~1, data=TZsurdat)
 
-SurCD4 <- survfit(Surv(as.numeric(cd4_date)-as.numeric(startdate), !is.na(cd4_date))~1, data=zerohack)
+SurCD4 <- survfit(Surv(as.numeric(cd4_date)-as.numeric(startdate), !is.na(cd4_date))~1, data=TZsurdat)
 
-SurEligible <- survfit(Surv(as.numeric(eligible_date)-as.numeric(startdate), !is.na(eligible_date))~1, data=zerohack)
+SurEligible <- survfit(Surv(as.numeric(eligible_date)-as.numeric(startdate), !is.na(eligible_date))~1, data=TZsurdat)
 
 
 plot(SurARV,col=1, xlab = "Time", ylab="Survival Probability",lty=1)
@@ -67,22 +53,18 @@ legend('topright',c("ARV","CD4","Eligibility"),col=c("black","red","green"),lty=
 
 
 SurARV_sex<-update(SurARV,.~sex)
-plot(SurARV_sex, xlab = "Time", ylab = "Survival Probability", lty=1,col=1:2)
+plot(SurARV_sex, xlab = "Time", ylab = "Survival Probability", lty=1,col=1:2,main="ARV Treatment")
+legend('topright',c("Male","Female"),col=c("black","red"),lty=1)
 
 SurEligible_sex <- update(SurEligible,.~sex)
-plot(SurEligible_sex, xlab = "Time", ylab = "Survival Probability", lty=1)
+plot(SurEligible_sex, xlab = "Time", ylab = "Survival Probability", lty=1:2, main="Eligiblility")
+legend('topright',c("Male","Female"),col=c("black","red"),lty=1)
 
 SurARV_whostage <- update(SurARV, . ~ base_whostage)
-plot(SurARV_whostage , col=c(1:4))
+plot(SurARV_whostage , col=c(1:4),xlab = "Time", ylab = "Survival Probability", main="WHO Stage")
+legend('topright',c("1","2","3","4"),col=c("black","red","green","blue"),lty=1)
 
 
-
-
-SurARV_crazy<-update(SurARV,.~age + start_year)
-
-mod <- coxph(Surv(as.numeric(arv_date)-as.numeric(hack1),!is.na(arv_date))~age + start_year,data=zerohack)
+mod <- coxph(Surv(as.numeric(arv_date)-as.numeric(hack1),!is.na(arv_date))~age + start_year,data=TZsurdat)
 summary(mod)
 
-plot(SurARV_crazy, xlab = "Time", ylab = "Survival Probability", lty=1)
-
-summary(SurARV)
