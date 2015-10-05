950577aa3edfbd5be234651bf5e170d0d6a87e59
mikelin2 <wzmichael.li@gmail.com>
2015-10-05 18:47:43 -0400

    LTFU added

--- firstdate.R
+++ firstdate.R
@@ -13,6 +13,11 @@ baseline <- function(cat){
   return(subset(cat, !is.na(cat))[1])
 }
 
+maxDate <- function(dates){
+  if(length(dates)==0)return((NA))
+  return(min(max(dates)+182.625,as.Date("2014-12-31")))
+}
+
 year <- function(date){
   if(length(date)==0) return((NA))
   return(as.numeric(format(date,"%Y")))
@@ -28,7 +33,8 @@ Datetable <- (summarise(group_by(eligible, patientid)
   , age = baseline(age)
   , sex = baseline(sex)
 	, firstVisit = minDate(visitdate)
-  , death_date = baseline(dateofdeath)
+  , LTFU_date = maxDate(visitdate)
+  , death_date = minDate(dateofdeath)
   , death = baseline(death)
 	, cd4_date = as.Date(minDate(subset(visitdate, !is.na(cd4))))
 	, eligible_date = minDate(subset(visitdate,
