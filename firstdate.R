394d8e8303d4874d988c0bc6d925f63c6ceb0ce3
mikelin2 <wzmichael.li@gmail.com>
2015-10-09 14:05:48 -0400

    clean up

--- firstdate.R
+++ firstdate.R
@@ -1,7 +1,9 @@
+library(dplyr)
+
 endDate <- as.Date("2014-12-31")
 year <- 365.25
 
-library(dplyr)
+##Date functions
 
 minDate <- function(dates){
 	if(length(dates)==0) return ((NA))
@@ -11,22 +13,6 @@ minDate <- function(dates){
 maxDate <- function(dates){
   if(length(dates)==0) return ((NA))
   return(max(dates))
-}
-
-baseline <- function(cat){
-	obs <- subset(cat,!is.na(cat))
-	if(length(obs)==0) return(NA)
-	return(obs[[1]])
-}
-
-baseline_delay <- function(cat, visits){
-	obsl <- !is.na(cat)
-	if(length(obsl)==0) return(NA)
-	return(visits[obsl][[1]] - visits[[1]])
-}
-
-first <- function(cat){
-	return(cat[[1]])
 }
 
 followUp <- function(dates){
@@ -39,34 +25,58 @@ dateYear <- function(date){
   return(as.numeric(format(date,"%Y")))
 }
 
-##summarise will not include the other columns in eligible.RData
-##combining baselines 
+first <- function(cat){
+  return(cat[[1]])
+}
 
-Datetable <- (summarise(group_by(eligible, patientid)
-	, first_age = first(age)
-	, sex = baseline(sex)
-	, firstVisit = minDate(visitdate)
-	, lastVisit = maxDate(visitdate)
-	, FUDate = followUp(visitdate)
-	, LTFU_status = FUDate < endDate
-	, death_date = minDate(dateofdeath)
-	, death = baseline(death)
-	, cd4_date = as.Date(minDate(subset(visitdate, !is.na(cd4))))
-	, eligible_date = minDate(subset(visitdate,
-		!is.na(eligible) & eligible))
-	, arv_date = minDate(subset(visitdate,
-		arvstatuscode == "Start ARV"  & !is.na(arvstatuscode)))
-	, base_cd4 = baseline(subset(cd4,!is.na(cd4))), sex_delay=baseline_delay(sex, visitdate)
-	, base_whostage = baseline(whostage)
-	, base_eligible = eligible[1] ## First eligibility status
-	, base_facility = baseline(hf_type)
-	, base_referred = baseline(referredfromid)
-	, base_TB = baseline(tbscreeningid)
-	, base_Cotrimoxazole = baseline(medication2)
-	, base_Malnourshed = baseline(nutritionalstatusid)
-) %>% mutate(start_year = dateYear(firstVisit)
- , cd4_year = dateYear(cd4_date)
- , eligible_year = dateYear(eligible_date)
- , arv_year = dateYear(arv_date)
-))
+##baseline functions
+
+baseline <- function(cat){
+	obs <- subset(cat,!is.na(cat))
+	if(length(obs)==0) return(NA)
+	return(obs[[1]])
+}
+
+baseline_delay <- function(cat, visits,logic=NULL){
+	obsl <- !is.na(cat)
+  if(!is.null(logic))(obsl <- !is.na(cat) & logic)
+	if(sum(obsl)==0) return(NA)
+	return(visits[obsl][[1]] - visits[[1]])
+}
+
+
+baseline_delay <- function(cat, visits){
+  obsl <- !is.na(cat)
+  if(length(obsl)==0) return(NA)
+  return(visits[obsl][[1]] - visits[[1]])
+}
+
+
+##summarise will not include the other columns in eligible.RData
+##Now we should create a Datetable AND a Baselinetable and merge at the end by patientid 
+
+patientdat <- eligible %>% group_by(patientid)
+
+Date_table <- (patientdat %>%
+                 summarise_each(funs(minDate,maxDate,followUp,
+                                     LTFU_status = followUp < endDate),visitdate)
+               )
+
+First_table <- (patientdat %>%
+                  summarise_each(funs(first), 
+                                 c(age,sex,death,eligible,whostage)
+                                 )
+                )
+
+Baseline_table <- (patientdat %>%
+                     summarise_each(funs(baseline),
+                      c(cd4,hf_type,referredfromid,tbscreeningid,nutritionalstatusid)
+                     )
+)
+                
+Baseline_delay <- patientdat  %>%
+  summarise_each(funs(baseline_delay(eligible,visitdate,eligible==TRUE),
+                      baseline_delay(cd4,visitdate,cd4>0),
+                      baseline_delay(arvstatuscode, visitdate,arvstatuscode == "Start ARV"))
+  )
 
