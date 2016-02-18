3bdd5c799c5e40b5e47ed336541d91ab3a783db1
mli2830 <wzmichael.li@gmail.com>
2016-02-18 08:47:27 -0500

    added some more plots

--- survival_plots.R
+++ survival_plots.R
@@ -165,7 +165,7 @@ ARTcoxfull <- (coxph(
 print(ARTcoxfull)
 
 
-## Linked and Gender ----
+## Linked by Gender ----
 LAsexSurv <- update(LASurv,.~sex_baseline)
 
 datLAsex <- data.frame(
@@ -173,12 +173,6 @@ datLAsex <- data.frame(
   nrisk = summary(LAsexSurv)$n.risk,
   events = summary(LAsexSurv)$n.event,
   gender = summary(LAsexSurv)$strata
-)
-
-
-datLAsex <- datLAsex %>% mutate(
-  surv = cumprod((nrisk-events)/nrisk),
-  followUp = time/year
 )
 
 datLAsex <- (datLAsex 
@@ -196,11 +190,101 @@ LAsexplot<- (ggplot(datLAsex, aes(followUp,surv,colour=gender))
           + geom_line() 
           + ggtitle("LTFU Through Time by gender")
           + ylab("S(t)")
+          + xlab("Year Lag")
           + theme_bw()
 )
 
 print(LAsexplot)
 
 
+## ART by Gender ----
+ARTsexSurv <- update(ARTSurv,.~sex_baseline)
 
+datARTsex <- data.frame(
+  time = summary(ARTsexSurv)$time,
+  nrisk = summary(ARTsexSurv)$n.risk,
+  events = summary(ARTsexSurv)$n.event,
+  gender = summary(ARTsexSurv)$strata
+)
+
+
+datARTsex <- (datARTsex 
+             %>% group_by(gender)
+             %>% mutate(
+               surv = cumprod((nrisk-events)/nrisk),
+               cumprob = 1 - surv,
+               followUp = time/year
+             )
+)
+
+ARTsexplot<- (ggplot(datARTsex, aes(followUp,cumprob,colour=gender))
+             + geom_line() 
+             + ggtitle("ART Through Time by gender")
+             + ylab("1-S(t)")
+             + xlab("Year Lag")
+             + theme_bw()
+)
+
+print(ARTsexplot)
+
+## ART by Eligibility ----
+ARTeliSurv <- update(ARTSurv,.~eligible_ever)
+
+datARTeli <- data.frame(
+  time = summary(ARTeliSurv)$time,
+  nrisk = summary(ARTeliSurv)$n.risk,
+  events = summary(ARTeliSurv)$n.event,
+  Eligible = summary(ARTeliSurv)$strata
+)
+
+
+datARTeli <- (datARTeli 
+              %>% group_by(Eligible)
+              %>% mutate(
+                surv = cumprod((nrisk-events)/nrisk),
+                cumprob = 1 - surv,
+                followUp = time/year
+              )
+)
+
+
+ARTeliplot<- (ggplot(datARTeli, aes(followUp,cumprob,colour=Eligible))
+              + geom_line() 
+              + ggtitle("ART Through Time by Eligibility")
+              + ylab("1-S(t)")
+              + xlab("Year Lag")
+              + theme_bw()
+)
+
+print(ARTeliplot)
+
+## ART by Enrolment year ----
+ARTyearSurv <- update(ARTSurv,.~enrolYear)
+
+datARTyear <- data.frame(
+  time = summary(ARTyearSurv)$time,
+  nrisk = summary(ARTyearSurv)$n.risk,
+  events = summary(ARTyearSurv)$n.event,
+  Enrolment_Year = summary(ARTyearSurv)$strata
+)
+
+
+datARTyear <- (datARTyear 
+              %>% group_by(Enrolment_Year)
+              %>% mutate(
+                surv = cumprod((nrisk-events)/nrisk),
+                cumprob = 1 - surv,
+                followUp = time/year
+              )
+)
+
+ARTyearplot<- (ggplot(datARTyear, aes(followUp,cumprob,colour=Enrolment_Year))
+              + geom_line() 
+              + ggtitle("ART Through Time by Enrolment Year")
+              + ylab("1-S(t)")
+              + xlab("Year Lag")
+              + theme_bw()
+)
+
+print(ARTyearplot)
 
