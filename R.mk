166fc16e9b61a9811f031358b5ab134045e0838f
Your Name <you@example.com>
2016-02-24 14:07:53 -0500

    Fixing spaces in R.mk

--- R.mk
+++ R.mk
@@ -62,10 +62,10 @@ coxph.%.Rout: survival.%.Rout coxph.R
 
 Sources += summary.Rnw
 %.tex: %.Rnw survival_plots.sample.Rout
-  echo "library('knitr'); knit(\"$*.Rnw\")" | R --slave
+	echo "library('knitr'); knit(\"$*.Rnw\")" | R --slave
 
 %.pdf: %.tex
-       texi2dvi -p $*.tex
+	texi2dvi -p $*.tex
 
 %.pdf.now: %.pdf
 	   evince summary.pdf
